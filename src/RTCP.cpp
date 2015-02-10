864e3a63c6eb8b06f44a325d644d2cf01fe32c50
Philipp Schößler <impanel@dhcp-18-111-7-103.dyn.mit.edu>
2015-02-10 15:17:20 -0500

    TCP tested with 3dsmax (working)

--- src/RTCP.cpp
+++ src/RTCP.cpp
@@ -2,6 +2,9 @@
 //  RTCP.cpp
 //  inform
 //
+//  Class to connect 3dsmax directl to the inFORM
+//
+//
 //  Created by Philipp Schoessler on 2/5/15.
 //
 //
@@ -18,12 +21,13 @@ void RTCP::setup(){
     pixels = new unsigned char[size];
     
     //setup the server to listen on 11999
-    TCP.setup(11996);
+    TCP.setup(11989);
     TCP.setMessageDelimiter("\n");
     
     //fill the vector<>
     for(int i = 0; i < size; i++)
     {
+        pixels[i] = 0;
         rects.push_back(ofRectangle());
         rects.at(i).setHeight(1);
         rects.at(i).setWidth(1);
@@ -49,40 +53,24 @@ void RTCP::update(float dt){
         if(!TCP.isClientConnected(i) )continue;
         
         //we only want to update the text we have recieved there is data
-        string str = TCP.receive(i);
+        string str = TCP.receive(i); //incoming data (30 * 30 8-bit values)
         
         if(str.length() > 0)
         {
+            //store data in vector for later plaback
             storeText.push_back(str);
-            unsigned char * receivedChars = new unsigned char[size];
             
+            //invert values here because we can't do it in maxscript
             for(int i = 0; i < size; i++)
-                receivedChars[i] = 255  - str[i]; //invert values here because it doesn't work in maxscript
-            pixels = receivedChars;
+                pixels[i] = 255  - str[i]; 
         }
     }
     
-    
-    //draw info about TCP
-    if(TCP.isClientConnected(0))
-    {
-        //calculate where to draw the text
-        int xPos = 506;
-        int yPos = 506;
-        
-        //get the ip and port of the client
-        string port = ofToString( TCP.getClientPort(0) );
-        string ip   = TCP.getClientIP(0);
-        string info = "client "+ofToString(0)+" -connected from "+ip+" on port: "+port;
-        string storeInfo = "stored frames: " + ofToString(storeText.size()) + " fIdx: " + ofToString(frameIndex);
-        
-        //draw the info text and the received text bellow it
-        ofDrawBitmapString(info, xPos, yPos);
-        ofDrawBitmapString(storeInfo, xPos + 500, yPos);
-    }
-    
+    //if no playback just show the most current data
     if (bStop)
         drawPins(pixels);
+    
+    // else plaback the animation
     else
         playBack(storeText, frameIndex, sequenceFPS, bPause, bStop);
 }
@@ -98,7 +86,7 @@ void RTCP::drawHeightMap()
 
 void RTCP::drawGraphics()
 {
-    //pinHeightImage.draw(506, 506, 500, 500);
+    //graphics for projector
 }
 
 //--------------------------------------------------------------
@@ -107,11 +95,26 @@ void RTCP::drawDebug(int _x, int _y)
 {
     pinHeightImage.draw(_x, _y, 500, 500);
     
-    //ofPushStyle();
-    ofSetColor(255, 0, 0);
-    string message = "TCP SERVER EasyPin 006 TRANSFORM \n\nconnect on port: "+ofToString(TCP.getPort());
-    ofDrawBitmapString(message, _x + 10, _y + 10);
-    //ofPopStyle();
+    //draw info about TCP
+    if(TCP.isClientConnected(0))
+    {
+        //ofPushStyle();
+        ofSetColor(255, 0, 0);
+        
+        //where to draw the text
+        int xPos = 6;
+        int yPos = 520;
+        
+        //get the ip and port of the client
+        string port = ofToString( TCP.getClientPort(0) );
+        string ip   = TCP.getClientIP(0);
+        string info = "client "+ofToString(0)+" -connected from "+ip+" on port: "+port;
+        string storeInfo = "stored frames: " + ofToString(storeText.size()) + " fIdx: " + ofToString(frameIndex);
+        
+        //draw the info text and the received text bellow it
+        ofDrawBitmapString(info, xPos, yPos);
+        ofDrawBitmapString(storeInfo, xPos, yPos + 35);
+    }
 }
 
 //--------------------------------------------------------------
@@ -129,7 +132,8 @@ void RTCP::playBack(vector <string> & _strFrames, int & _frameIndex, int _sequen
             unsigned char * theColors = new unsigned char[size];
             for(int i = 0; i < size; i++)
             {
-                theColors[i] = 255  - _strFrames[_frameIndex][i]; //invert values here because it doesn't work in maxscript
+                //invert values here because we can't do it in maxscript
+                theColors[i] = 255  - _strFrames[_frameIndex][i];
             }
             drawPins(theColors);
         }
@@ -140,26 +144,20 @@ void RTCP::playBack(vector <string> & _strFrames, int & _frameIndex, int _sequen
 
 void RTCP::drawPins(unsigned char * _theColors)
 {
-    
-//    cout<<"called"<<endl;
-//    //debug
-    for (int i = 0; i < 30 * 30; i++) {
-        _theColors[i] = (int)ofMap(i, 0, 899, 0, 255);
-    }
-
-    int rows = 30;
-    int columns = 30;
+    int rows = RELIEF_SIZE_X;
+    int columns = RELIEF_SIZE_Y;
         
     pinHeightImage.begin();
     ofClear(255,255,255, 0);
-    
     
     //sort incoming data
     for(int i = 0; i < columns; i++) // columns
     {
         for(int j = 0; j < rows; j++) // rows
         {
-            int idx = i * columns +j; //2D to 1D array
+            //2D to 1D array
+            int idx = i * columns +j; 
+            
             rects.at(idx).setWidth(1);
             rects.at(idx).setHeight(1);
             
@@ -167,6 +165,7 @@ void RTCP::drawPins(unsigned char * _theColors)
             rects.at(idx).y = j*1;
             
             ofSetColor(_theColors[idx]);
+            
             //draw values in sorted order
             ofRect(rects.at(idx));
             //cout<<idx << " : " <<(int)_theColors[idx]<<endl;
@@ -206,19 +205,34 @@ void RTCP::clearFrames()
 
 void RTCP::keyPressed(int key)
 {
+    //clear frame arra
     if (key == 'c')
+        bStop = true;
+        frameIndex = 0;
+        for(int i = 0; i < size; i++)
+            pixels[i] = 0;
         storeText.clear();
-    if (key == 'x') //toggle pause
+    
+    //toggle pause
+    if (key == ' ') 
     {
-        cout<<"pause"<<endl;
         bPause = !bPause;
         bStop = false;
         if(bPause)
+        {
+            cout<<"pause"<<endl;
             pauseTime = ofGetElapsedTimef() - elapsedTime;
+        }
         else
+        {
+            cout<<"play"<<endl;
             elapsedTime = ofGetElapsedTimef() - pauseTime;
+        }
+
     }
-    if (key == 'z') //toggle stop
+    
+    //toggle stop
+    if (key == 'z') 
     {
         //stop
         bStop = !bStop;
